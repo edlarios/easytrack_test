695d375c2dd93e16017e29505822d4fe37234b0a
edlarios <eduardo.larios@proton.me>
2023-02-25 09:32:59 +0000

    Remove provisioners

--- main.tf
+++ main.tf
@@ -14,25 +14,10 @@ resource "aws_instance" "easytr-machine" {
     Name = "easytr-machine-${count.index}"
   }
 
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("~/.ssh/id_rsa")
-    host        = self.public_ip
   }
-  // Instalacion de NGINX y cambiar mensaje
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo yum install nginx -y",
-      "sudo sh -c 'echo \"Hello, Alberto Barba!\" > /usr/share/nginx/html/index.html'",
-      "sudo systemctl start nginx"
-    ]
-  }
-}
 
 resource "aws_key_pair" "easytrack-test-key" {
   key_name   = "easytrack-test-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = var.sample_public_key
 }
 
